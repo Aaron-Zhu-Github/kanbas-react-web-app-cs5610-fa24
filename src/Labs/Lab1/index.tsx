c40a631884e6ed36d585b8d037cd21ec4522a3c6
Aaron <jz795@scarletmail.rutgers.edu>
2024-09-18 02:22:54 -0700

    fix3

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -5,66 +5,66 @@ export default function Lab1() {
         <h3>HTML Examples</h3>
         <div id="wd-h-tag">
             <h4>Heading Tags</h4>
-Text documents are often broken up into several sections and
-subsections. Each section is usually prefaced with a short
-title or heading that attempts to summarize the topic of the
-section it precedes. For instance this paragraph is preceded by
-the heading Heading Tags. The font of the section headings are
-usually larger and bolder than their subsection headings. This
-document uses headings to introduce topics such as HTML
-Documents, HTML Tags, Heading Tags, etc. HTML heading tags can
-be used to format plain text so that it renders in a browser as
-large headings. There are 6 heading tags for different sizes:
-h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and
-h6 is the smallest heading.
-    </div>
-    <div id="wd-p-tag">
-        <h4>Paragraph Tag</h4>
-        <p id="wd-p-1">
-This is a paragraph. We often separate a long set of sentences with
-vertical spaces to make the text easier to read. Browsers ignore vertical
-white spaces and render all the text as one single set of sentences. To
-force the browser to add vertical spacing, wrap the paragraphs you want to
-separate with the paragraph tag
-</p> 
-        <p id="wd-p-2">
-This is the first paragraph. The paragraph tag is used to format
-vertical gaps between long pieces of text like this one.
-        </p>
-        <p id="wd-p-3">
-This is the second paragraph. Even though there is a deliberate white
-gap between the paragraph above and this paragraph, by default
-browsers render them as one contiguous piece of text as shown here on
-the right.
-        </p>
-        <p id="wd-p-4">
-This is the third paragraph. Wrap each paragraph with the paragraph
-tag to tell browsers to render the gaps.
-        </p>
+            Text documents are often broken up into several sections and
+            subsections. Each section is usually prefaced with a short
+            title or heading that attempts to summarize the topic of the
+            section it precedes. For instance this paragraph is preceded by
+            the heading Heading Tags. The font of the section headings are
+            usually larger and bolder than their subsection headings. This
+            document uses headings to introduce topics such as HTML
+            Documents, HTML Tags, Heading Tags, etc. HTML heading tags can
+            be used to format plain text so that it renders in a browser as
+            large headings. There are 6 heading tags for different sizes:
+            h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and
+            h6 is the smallest heading.
+        </div>
+        <div id="wd-p-tag">
+            <h4>Paragraph Tag</h4>
+            <p id="wd-p-1">
+                This is a paragraph. We often separate a long set of sentences with
+                vertical spaces to make the text easier to read. Browsers ignore vertical
+                white spaces and render all the text as one single set of sentences. To
+                force the browser to add vertical spacing, wrap the paragraphs you want to
+                separate with the paragraph tag
+            </p> 
+            <p id="wd-p-2">
+                This is the first paragraph. The paragraph tag is used to format
+                vertical gaps between long pieces of text like this one.
+            </p>
+            <p id="wd-p-3">
+                This is the second paragraph. Even though there is a deliberate white
+                gap between the paragraph above and this paragraph, by default
+                browsers render them as one contiguous piece of text as shown here on
+                the right.
+            </p>
+            <p id="wd-p-4">
+                This is the third paragraph. Wrap each paragraph with the paragraph
+                tag to tell browsers to render the gaps.
+            </p>
         </div>
         <div id="wd-lists">
             <h4>List Tags</h4>
             <h5>Ordered List Tag</h5>
             How to make pancakes:
             <ol id="wd-pancakes">
-            <li>Mix dry ingredients.</li>
-            <li>Add wet ingredients.</li>
-            <li>Stir to combine.</li>
-            <li>Heat a skillet or griddle.</li>
-            <li>Pour batter onto the skillet.</li>
-            <li>Cook until bubbly on top.</li>
-            <li>Flip and cook the other side.</li>
-            <li>Serve and enjoy!</li>
+                <li>Mix dry ingredients.</li>
+                <li>Add wet ingredients.</li>
+                <li>Stir to combine.</li>
+                <li>Heat a skillet or griddle.</li>
+                <li>Pour batter onto the skillet.</li>
+                <li>Cook until bubbly on top.</li>
+                <li>Flip and cook the other side.</li>
+                <li>Serve and enjoy!</li>
             </ol>
             My favorite recipe: Home Made Omelet
             <ol id="wd-your-favorite-recipe">
-            <li>Crack eggs into a bowl with salt and pepper, and whisk.</li>
-            <li>Heat butter in a pan.</li>
-            <li>Fry ham or other meat for two minutes.</li>
-            <li>Fry vagetable for one minuts.</li>
-            <li>Pour the egg mixture into the pan and cook gently.</li>
-            <li>Fold the omelet in half and cook for another minute.</li>
-            <li>Serve hot with your favorite toppings and sauces and enjoy!</li>
+                <li>Crack eggs into a bowl with salt and pepper, and whisk.</li>
+                <li>Heat butter in a pan.</li>
+                <li>Fry ham or other meat for two minutes.</li>
+                <li>Fry vegetables for one minute.</li>
+                <li>Pour the egg mixture into the pan and cook gently.</li>
+                <li>Fold the omelet in half and cook for another minute.</li>
+                <li>Serve hot with your favorite toppings and sauces and enjoy!</li>
             </ol>
             <h5>Unordered List Tag</h5>
             My favorite books (in no particular order)
@@ -171,14 +171,12 @@ tag to tell browsers to render the gaps.
             <br />
             <img id="wd-starship"
                 width="400px"
-
-        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1
-            _Starship-gap2.jpg"
-            alt="Starship Image" />
+                src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+                alt="Starship from Star Advertiser" />
             <br />
             Loading a local image:
             <br />
-            <img id="wd-teslabot" src="images/teslabot.jpg " height="200px" alt="Tesla Bot image" />
+            <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="Tesla Bot" />
         </div>
         <div id="wd-forms">
             <h4>Form Elements</h4>
@@ -198,47 +196,47 @@ tag to tell browsers to render the gaps.
                 <h5>Text boxes</h5>
                 <label>Biography:</label><br/>
                 <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                 cupidatat non proident, sunt in culpa qui officia deserunt mollit
                 anim id est laborum.</textarea>
 
-                <h5 id="wd-buttons">Buttons</h5>
+                <h5>Buttons</h5>
                 <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
                     Hello World!
                 </button>
 
                 <h5>File upload</h5>
-                <input id="wd-upload" type="file"/>
+                <input id="wd-upload" type="file" />
 
-                <h5 id="wd-radio-buttons">Radio buttons</h5>
+                <h5>Radio buttons</h5>
                 <label>Favorite movie genre:</label><br />
-                <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+                <input type="radio" name="radio-genre" id="wd-radio-comedy" />
                 <label htmlFor="wd-radio-comedy">Comedy</label><br />
-                <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+                <input type="radio" name="radio-genre" id="wd-radio-drama" />
                 <label htmlFor="wd-radio-drama">Drama</label><br />
-                <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+                <input type="radio" name="radio-genre" id="wd-radio-scifi" />
                 <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
-                <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+                <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
                 <label htmlFor="wd-radio-fantasy">Fantasy</label>
 
-                <h5 id="wd-checkboxes">Checkboxes</h5>
-                <label>Favorite movie genre:</label><br/>
-                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
-                <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
-                <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
-                <label htmlFor="wd-chkbox-drama">Drama</label><br/>
-                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
-                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
-                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+                <h5>Checkboxes</h5>
+                <label>Favorite movie genre:</label><br />
+                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
+                <label htmlFor="wd-chkbox-comedy">Comedy</label><br />
+                <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
+                <label htmlFor="wd-chkbox-drama">Drama</label><br />
+                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
+                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br />
+                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
                 <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
 
-                <h4 id="wd-dropdowns">Dropdowns</h4>
+                <h4>Dropdowns</h4>
                 <h5>Select one</h5>
-                <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+                <label htmlFor="wd-select-one-genre">Favorite movie genre:</label><br />
                 <select id="wd-select-one-genre">
                     <option value="COMEDY">Comedy</option>
                     <option value="DRAMA">Drama</option>
@@ -246,47 +244,35 @@ tag to tell browsers to render the gaps.
                     <option value="FANTASY">Fantasy</option>
                 </select>
                 <h5>Select many</h5>
-                <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+                <label htmlFor="wd-select-many-genre">Favorite movie genres:</label><br />
                 <select id="wd-select-many-genre" multiple>
                     <option selected value="COMEDY">Comedy</option>
                     <option value="DRAMA">Drama</option>
-                    <option selected value="SCIFI">
-                        Science Fiction</option>
+                    <option selected value="SCIFI">Science Fiction</option>
                     <option value="FANTASY">Fantasy</option>
                 </select>
 
                 <h4>Other HTML field types</h4>
-                <label htmlFor="wd-text-fields-email"> Email: </label>
-                <input type="email"
-                    placeholder="jdoe@somewhere.com"
-                    id="wd-text-fields-email"/><br/>
-                <label htmlFor="wd-text-fields-salary-start"> Starting salary:
-                </label>
-                <input type="number"
-                    id="wd-text-fields-salary-start"
-                    placeholder="1000"
-                    value="100000"/><br/>
-                <label htmlFor="wd-text-fields-rating"> Rating: </label>
-                <input type="range" id="wd-text-fields-rating"
-                    placeholder="Doe"
-                    max="5"
-                    value="4"/><br/>
-                <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
-                <input type="date"
-                    id="wd-text-fields-dob"
-                    value="2000-01-21"/><br/>
+                <label htmlFor="wd-text-fields-email">Email:</label>
+                <input type="email" placeholder="jdoe@somewhere.com" id="wd-text-fields-email" /><br />
+                <label htmlFor="wd-text-fields-salary-start">Starting salary:</label>
+                <input type="number" id="wd-text-fields-salary-start" placeholder="1000" value="100000" /><br />
+                <label htmlFor="wd-text-fields-rating">Rating:</label>
+                <input type="range" id="wd-text-fields-rating" placeholder="Doe" max="5" value="4" /><br />
+                <label htmlFor="wd-text-fields-dob">Date of birth:</label>
+                <input type="date" id="wd-text-fields-dob" value="2000-01-21" /><br />
 
                 <h4>Anchor tag</h4>
                 Please
                 <a id="wd-lipsum" href="https://www.lipsum.com">click here</a>
-                to get dummy text<br/>
+                to get dummy text<br />
                 Please
                 <a id="wd-github" href="https://github.com/Aaron-Zhu-Github?tab=repositories">click here</a>
-                to my GitHub code repository<br/>
+                to my GitHub code repository<br />
 
                 {/* copy rest of form elements here  */}
             </form>
         </div>
-  </div>
-  ); 
+      </div>
+    );
 }
