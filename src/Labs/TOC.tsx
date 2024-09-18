bd7dc2c4210e2e8d1a37482ce2a3b939ac67d798
Aaron <jz795@scarletmail.rutgers.edu>
2024-09-18 12:27:45 -0700

    fix4

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,7 +4,7 @@ export default function TOC() {
     <ul>
       <li>
         <Link id="wd-a" to="/Labs">
-          Labs
+          Landing Page
         </Link>
       </li>
       <li>
@@ -30,19 +30,3 @@ export default function TOC() {
     </ul>
   );
 }
-
-
-// import { Link } from "react-router-dom";
-// export default function TOC() {
-//     return (
-//       <ul>
-//         <li><a id="wd-a"  href="#/Labs">Labs</a></li>
-//         <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
-//         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
-//         <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
-//         <li><a id="wd-k"  href="#/Kanbas">Kanbas</a></li>
-//         {/* Method 2 for implementing navigation in Single Page Application  */}
-//         {/* <li><a id="wd-k"  href="#/Kanbas/Dashboard">Kanbas</a></li> */}
-
-//   </ul>
-//   ); }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router-dom";
 import TOC from "./TOC";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
@@ -6,10 +6,14 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Labs</h1>
+      <div>
+        <h3>Name: Junren Zhu</h3>
+        <h3>Section: 02</h3>
+      </div>
+      <h1>Landing Page</h1>
       <TOC />
       <Routes>
-        {/* <Route path="/" element={<Navigate to="Lab1" />} /> */}
+        <Route path="/" element={<Navigate to="Labs" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
