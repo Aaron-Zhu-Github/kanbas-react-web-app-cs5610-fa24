fac66e661902b7622299836344d4957d2c73b491
Aaron <jz795@scarletmail.rutgers.edu>
2024-10-03 23:29:19 -0700

    fix14

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -3,18 +3,15 @@ import ReactIconsSampler from "./ReactIcons";
 import ScreenSizeLabel from "./ScreenSizeLabel";
 export default function Lab2() {
     return (
-      // <div id="wd-lab2">
       <div id="container">
         <h2>Lab 2 - Cascading Style Sheets</h2>
         <h3>Styling with the STYLE attribute</h3>
-        {/* <p style={{ backgroundColor: "blue", color: "white" }}> */}
         <div id="wd-css-id-selectors">
           <p id="wd-id-selector-0">
           Style attribute allows configuring look and feel
           right on the element. Although it's very convenient
           it is considered bad practice and you should avoid
           using the style attribute
-        {/* </p>  */}
           </p>
         </div>
 
