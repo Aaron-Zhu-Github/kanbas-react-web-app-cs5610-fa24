70d19bd57f8858dffb3e42cc849a3d628cf290ca
Aaron <jz795@scarletmail.rutgers.edu>
2024-10-15 13:31:09 -0700

    fix25

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 // All images in this assignment are from internet
 import { Link } from "react-router-dom";
 import * as db from "../Database";
+
 export default function Dashboard() {
   const courses = db.courses;
   return (
@@ -9,12 +10,12 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          {courses.map((course) => (
+          {courses.map((course) => ( 
             <div className="wd-dashboard-course col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden">
                 <Link to={`/Kanbas/Courses/${course._id}/Home`}
                       className="wd-dashboard-course-link text-decoration-none text-dark" >
-                  <img src={course.image} width="100%" height={160} alt={course.name} />
+                  <img src={`/images/${course._id}.png`} width="100%" height={160} alt={course.name} />
                   <div className="card-body">
                     <h5 className="wd-dashboard-course-title card-title">
                       {course.name}
