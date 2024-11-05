1e370671a5837a73c0cc2ab3a614fb07e44e51c8
Aaron <jz795@scarletmail.rutgers.edu>
2024-11-05 03:11:17 -0800

    fix30

--- src/Kanbas/Courses/Assignments/reducer.tsx
+++ src/Kanbas/Courses/Assignments/reducer.tsx
@@ -1,36 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { assignments as initialAssignments } from "../../Database";
-
-// const initialState = {
-//   assignments: initialAssignments,
-// };
-
-// const assignmentsSlice = createSlice({
-//   name: "assignments",
-//   initialState,
-//   reducers: {
-//     addAssignment: (state, { payload }) => {
-//       const newAssignment = {
-//         _id: new Date().getTime().toString(),
-//         ...payload,
-//       };
-//       state.assignments.push(newAssignment);
-//     },
-//     deleteAssignment: (state, { payload: assignmentId }) => {
-//       state.assignments = state.assignments.filter(
-//         (assignment) => assignment._id !== assignmentId
-//       );
-//     },
-//     editAssignment: (state, { payload }) => {
-//       const index = state.assignments.findIndex((a) => a._id === payload._id);
-//       if (index >= 0) state.assignments[index] = payload;
-//     },
-//   },
-// });
-
-// export const { addAssignment, deleteAssignment, editAssignment } = assignmentsSlice.actions;
-// export default assignmentsSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit'
 import { assignments } from '../../Database'
 
